418471f7571bd643d79142d4e8e59a0ee5006c0a
Rik Verhoeven <rikverhoeven10@gmail.com>
2023-12-15 11:28:45 +0100

    p

--- day3/main.c
+++ day3/main.c
@@ -8,78 +8,114 @@
 # include <string.h>
 # include "get_next_line.h"
 
-
-
-
-int look_for_symbols(char **total, int *i, int tmp_j)
+int ifnum(char chr)
 {
-	if(total[*i + 1] != NULL)
-		if(tmp_j < strlen(total[*i + 1]))
-			if(total[*i + 1][tmp_j] >= '!' && total[*i][tmp_j] <= '/')
-				return (1);
-	if(*i >= 1)
-		if(total[*i - 1][tmp_j] >= '!' && total[*i - 1][tmp_j] <= '/')
-			return (1);
-	if(tmp_j >= 1)
-		if(total[*i][tmp_j - 1] >= '!' && total[*i][tmp_j - 1] <= '/')
-			return (1);
-	int len = strlen(total[*i]);
-	if(tmp_j < len)
-		if(total[*i][tmp_j + 1] >= '!' && total[*i][tmp_j + 1] <= '/')
-			return (1);
-	if(*i >= 1 && tmp_j >= 1)
-		if(total[*i - 1][tmp_j - 1] >= '!' && total[*i - 1][tmp_j - 1] <= '/')
-			return (1);
-	if(tmp_j < len && total[*i + 1] != NULL)
-		if(total[*i + 1][tmp_j + 1] >= '!' && total[*i + 1][tmp_j + 1] <= '/')
-			return (1);
+	if(chr >= '0' && chr <= '9')
+		return (1);
 	return (0);
 }
 
+int ifsymb(char chr)
+{
+	if(chr == '.')
+		return (0);
+	if(chr >= '!' && chr <= '/')
+		return (1);
+	if(chr >= ':' && chr <= '@')
+		return (1);
+	return (0);
+}
+
+int look_for_symbols(char **total, int *i, int tmp_j)
+{
+	int len = strlen(total[*i]);
+	//onder
+	if(total[*i + 1] != NULL)
+		if(ifsymb(total[*i + 1][tmp_j]))
+			return (1);
+	//boven
+	if(*i >= 1)
+		if(ifsymb(total[(*i) - 1][tmp_j]))
+			return (1);
+	//links
+	if(tmp_j >= 1)
+		if(ifsymb(total[*i][tmp_j - 1]))
+			return (1);
+	//rechts
+	if(tmp_j < len)
+		if(ifsymb(total[*i][tmp_j + 1]))
+			return (1);
+	//linksonder
+	if(tmp_j >= 1 && total[*i + 1] != NULL)
+		if(ifsymb(total[*i + 1][tmp_j - 1]))
+			return (1);
+	//linksboven
+	if(*i >= 1 && tmp_j >= 1)
+		if(ifsymb(total[*i - 1][tmp_j - 1]))
+			return (1);
+	//rechtsonder
+	if(tmp_j < len && total[*i + 1] != NULL)
+		if(ifsymb(total[*i + 1][tmp_j + 1]))
+			return (1);
+	//rechtsboven
+	if(tmp_j < len && *i >= 1)
+		if(ifsymb(total[*i - 1][tmp_j + 1]))
+			return (1);
+
+	return (0);
+}
 
 int parse_engine_part(char **total, int *i)
 {
     int tmp_j;
-    int j;
 	int symbol_seen;
 	int nbr;
 	int total_nbr;
+
 	total_nbr = 0;
 	nbr = 0;
 	symbol_seen = 0;
-    j = 0;
     tmp_j = 0;
-
 	while(total[*i][tmp_j] != '\0')
 	{
 		while(total[*i][tmp_j] >= '0' && total[*i][tmp_j] <= '9')
 		{
 			if(tmp_j == 0)
-				nbr = atoi(total[*i]);
+				nbr = atoi(&total[*i][tmp_j]);
 			else if(!(total[*i][tmp_j - 1] >= '0' && total[*i][tmp_j - 1] <= '9'))
-				nbr = atoi(total[*i]);
-			symbol_seen = look_for_symbols(total, i, tmp_j);
-			if(symbol_seen && tmp_j == strlen(total[*i]) - 1)
-				total_nbr += nbr;
-			else if(symbol_seen && !(total[*i][tmp_j + 1] >= '0' && total[*i][tmp_j + 1] <= '9'))
-				total_nbr += nbr;
+				nbr = atoi(&total[*i][tmp_j]);
+			if(!symbol_seen)
+				symbol_seen = look_for_symbols(total, i, tmp_j);
+			look_for_symbols(total, i, tmp_j);
 			tmp_j++;
 		}
+		if(symbol_seen)
+			printf("%d symbol? %d \n", nbr, symbol_seen);
+		if(!symbol_seen)
+			nbr = 0;
+		total_nbr += nbr;
+		nbr = 0;
+		symbol_seen = 0;
 		tmp_j++;
 	}
 	return (total_nbr);
 }
+// printf("%d symbol? %d \n", nbr, symbol_seen);
+
+
+typedef struct p
+{
+	int parts_cnt;
+} point;
 
 int main(int argc, char **argv)
 {
     int fd = open(argv[1], O_RDONLY);
-
     char *totalstr[15000];
+	point *totalstr_cnt[15000];
     char *next_line;
 	fd = open(argv[1], O_RDONLY);
 	next_line = (char *)1;
-
-
     int j;
 
     j = 0;
@@ -87,25 +123,22 @@ int main(int argc, char **argv)
 	while(1)
 	{
         totalstr[j] = get_next_line(fd);
+		// totalstr_cnt[j] = calloc(sizeof(point), strlen(totalstr[j]));
         if(totalstr[j] == NULL)
             break ;
-        printf("%s strlen %d", totalstr[j], (int)strlen(totalstr[j]));
+        // printf("%s strlen %d", totalstr[j], (int)strlen(totalstr[j]));
         j++;
 	}
-    printf("%s strlen \n\n", totalstr[j]);
-
-
+    // printf("%s strlen \n\n", totalstr[j]);
     int total_engine_parts;
     total_engine_parts = 0;
     j = 0;
     while(totalstr[j] != NULL)
     {
-        // if(totalstr[j] >= '0' && totalstr[j] <= '9')
             total_engine_parts += parse_engine_part(totalstr, &j);
-        // else
             j++;
     }
-	 printf("%d engineparts\n\n", total_engine_parts);
+	printf("%d engineparts\n\n", total_engine_parts);
     j = 0;
     while(1)
     {
